7dd7304221da8fa4065c187c4ff9fa93b4aaccfa
fedorzvyk <fedorzvyk@gmail.com>
2023-04-23 12:08:34 +0300

    bound tasks with users

--- utils/taskUtils.js
+++ utils/taskUtils.js
@@ -1,30 +1,33 @@
 const Task = require('../models/taskModel');
+const { NotFound } = require('http-errors');
 
 const createTask = async body => {
   const newTask = await Task.create(body);
   return newTask;
 };
 
-const findTasks = async (year, month) => {
-  const tasks = await Task.find({ year, month });
+const findTasks = async (owner, year, month) => {
+  const tasks = await Task.find({ owner, year, month });
   if (tasks.length <= 0) {
-    throw new Error('not find');
+    throw new NotFound('Tasks have not been found');
   }
   return tasks;
 };
 
 const removeTask = async id => {
   const task = await Task.findByIdAndRemove(id);
-  if (!task) throw new Error(`Not found`);
+  if (!task) throw new NotFound('Task has not been found');
 
   return task;
 };
 
 const updateTaskById = async (id, body) => {
-  const task = await Task.findByIdAndUpdate(id, { ...body }, { new: true });
-
-  if (!task) throw new Error(`Not found `);
-  return task;
+  try {
+    const task = await Task.findByIdAndUpdate(id, { ...body }, { new: true });
+    return task;
+  } catch (error) {
+    throw new NotFound('Task has not been found');
+  }
 };
 
 module.exports = {
